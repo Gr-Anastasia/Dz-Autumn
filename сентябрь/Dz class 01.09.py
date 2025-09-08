fc4f014b6fe7f018972adb44ab56c3644c958c15
Nastya Gr <proekt28n@gmail.com>
2025-09-08 23:38:37 +0500

    Fix bag in DZ 01.09

--- сентябрь/Dz class 01.09.py
+++ сентябрь/Dz class 01.09.py
@@ -1,13 +1,13 @@
 class Song:
-    def __init__(self,name, author, year):
+    def __init__(self, name, author, year):
         self.name = name
         self.author = author
         self.year = year
 
 class Album:
-    def __init__(self, name, author):
+    def __init__(self, name, year):
         self.name = name
-        self.author = author
+        self.year = year
         self.music = list()
 
     def add_song(self, song):
@@ -15,12 +15,10 @@ class Album:
 
     def get_song(self):
         return [f" Название: {song.name} | Автор: {song.author}" for song in self.music]
-        # for song in self.music:
-        #     print(f"Название: {song.name} \n Автор: {song.author}")
+
 
     def get_uniq_author(self):
         uniq_author = set()
-        # return [uniq_author.add(song.author) for song in self.music]
         for song in self.music:
             uniq_author.add(song.author)
         return uniq_author
@@ -33,38 +31,36 @@ class Album:
 
 class MusicLibrary:
     def __init__(self):
-        self.sound = list()
+        self.album = list()
 
     def add_album(self, album):
-        self.sound.append(album)
+        self.album.append(album)
 
     def get_author_album(self, author):
         author_songs = set()
-        for album in self.sound:
+        for album in self.album:
             for song in album.music:
                 if song.author == author:
                     author_songs.add(song)
-        return author_songs
+        return [song.name for song in author_songs]
+
 
     def get_uniq_music_album(self):
-        # music_set = [set(album.music()) for album in self.sound] НЕ РАБОТАЕТ
         music_set = []
-        for album in self.sound:
+        for album in self.album:
             y = set(album.music)
             music_set.append(y)
         uniq_music = set.intersection(*music_set)
-        return uniq_music
-
-
+        return [music.name for music in uniq_music]
 
 song_1 = Song("Smile", "Wolf Alice", "2021")
 song_2 = Song("Love Story", "KUTE", "2025")
 song_3 = Song("Rock That Body", "Black Eyed Peas", "2010")
 song_4 = Song("Storms", "Wolf Alice", "2014")
 
-album_1 = Album("Fovorits", "Disco")
-album_2 = Album("Party", "Violet")
-album_3 = Album("The road", "Melon")
+album_1 = Album("Fovorits", "2025")
+album_2 = Album("Party", "2022")
+album_3 = Album("The road", "2018")
 
 favorits_album = MusicLibrary()
 
@@ -80,14 +76,13 @@ favorits_album.add_album(album_1)
 favorits_album.add_album(album_2)
 favorits_album.add_album(album_3)
 
-# print("Песни в альбоме", album_1.get_song())
-# print("Песни в альбоме: ", album_2.get_song())
-# print("Уникальные авторы в альбоме: ", album_2.get_uniq_author())
-# print("Поиск 3 песни в альбоме: ", album_2.find_song(song_3))
-# print("Поиск 2 песни в альбоме: ", album_2.find_song(song_2))
+print("Песни в альбоме", album_1.get_song())
+print("Песни в альбоме: ", album_2.get_song())
+print("Уникальные авторы в альбоме: ", album_2.get_uniq_author())
+print("Поиск 3 песни в альбоме: ", album_2.find_song(song_3))
+print("Поиск 2 песни в альбоме: ", album_2.find_song(song_2))
 
-# print("Все песни автора KUTE: ", favorits_album.get_author_album(song_2.author))
-# я правда не понимаю почему выводиться ссылка на экземпляр класса
+print("Все песни автора KUTE: ", favorits_album.get_author_album(song_2.author))
 print("Песня song_3 во всех альбомов: ", favorits_album.get_uniq_music_album())
 
 
