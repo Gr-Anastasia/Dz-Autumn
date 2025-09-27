3545e1884d4e65b6ca9ec51fcd2b0cc52e54ef56
Nastya Gr <proekt28n@gmail.com>
2025-09-27 22:46:58 +0500

    fix bag (decoration time and main_list) DZ class cat

---
+++ сентябрь/class cat/Main.py
@@ -0,0 +1,13 @@
+from SmallCat import SmallCat
+from BigCat import BigCat
+
+max_bro = BigCat()
+mini_bro = SmallCat()
+
+max_bro.sleep()
+max_bro.eat()
+max_bro.run()
+
+mini_bro.sleep()
+mini_bro.eat()
+mini_bro.run()
